6f8a3b7fbc86c1043397b2706bfd5dd0f5ceeadc
minkinen <minkinen@yahoo.com>
2022-05-13 17:07:53 +0200

    A test to see if I can make a branch. This version is for the MVC Basics, Assignment 1 - Views and Controllers

--- MVC_Basics/Controllers/HomeController.cs
+++ MVC_Basics/Controllers/HomeController.cs
@@ -8,5 +8,20 @@ namespace MVC_Basics.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
     }
 }
